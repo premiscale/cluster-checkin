d9fc0269be38ef0370646743e195e2c59fd149da
Emma Doyle <emma@sbevision.com>
2023-09-10 09:10:20 -0400

    master: updates

--- Dockerfile
+++ Dockerfile
@@ -7,13 +7,18 @@ ENV CRONITOR_TELEMETRY_KEY=""
 # Install the Doppler CLI via apt for secrets retrieval.
 RUN apt-get update \
     && apt-get install -y curl unzip \
-    && rm -rf /var/lib/apt/lists/*
+    && rm -rf /var/lib/apt/lists/* \
+    && useradd -rm -d /opt/cronitor -s /bin/bash -g cronitor -G sudo -u 1001 cronitor
 
 RUN curl -sOL "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
     install kubectl /usr/bin/kubectl && \
     rm kubectl
 
 WORKDIR /app
-COPY scripts/run.sh .
+COPY bin/run.sh .
+RUN chown -R cronitor:cronitor /app \
+    && chmod +x run.sh
+
+USER cronitor
 
 CMD [ "./run.sh" ]
---
+++ bin/run.sh
@@ -0,0 +1,51 @@
+#! /usr/bin/env bash
+# Wrap tests in the cronitor.io API.
+
+
+set -o pipefail
+shopt -s nullglob
+
+
+# Service will not start without these environment variables.
+_CRONITOR_TELEMETRY_KEY="${CRONITOR_TELEMETRY_KEY:?}"
+_CLUSTER_ID="${CLUSTER_ID:?}"
+
+
+##
+# Fail if any nodes are not ready.
+test_nodes_not_ready()
+{
+    kubectl get nodes
+
+    kubectl get nodes | grep -qP "(NotReady)"
+
+    # Invert the last exit code.
+    test $? -eq 1
+}
+
+
+##
+# Test if any pods in namespaces are not up and running.
+test_pods_namespace()
+{
+    if [ $# -ne 1 ]; then
+        printf "Function \"test_pods\" expected 1 argument: namespace name." >&2
+        exit 1
+    fi
+
+    local namespace="$1"
+
+    kubectl get pods -n "$namespace"
+
+    kubectl get pods -n "$namespace" | grep -qP "(Error|ImagePullBackoff|PostStartHookError|PreStopHookError|InvalidImageName)"
+
+    # Invert the last exit code.
+    test $? -eq 1
+}
+
+
+(curl -s "https://cronitor.link/p/$_CRONITOR_TELEMETRY_KEY/$_CLUSTER_ID?state=run" && (
+    test_nodes_not_ready && \
+    test_pods_namespace "kube-system" && \
+    curl -s "https://cronitor.link/p/$_CRONITOR_TELEMETRY_KEY/$_CLUSTER_ID?state=complete"
+)) || curl -s "https://cronitor.link/p/$_CRONITOR_TELEMETRY_KEY/$_CLUSTER_ID?state=fail"
